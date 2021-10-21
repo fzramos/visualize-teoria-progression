c6cf58e4aa7a48a0c146e78dfd57a48258e03d20
Fred Ramos <fzramos6@gmail.com>
2021-10-21 09:45:54 -0500

    Trendline graph for overall progress added and changing the look of the dashboard

--- visualize_training_results.py
+++ visualize_training_results.py
@@ -18,26 +18,26 @@ def run_viz_app():
     print(df_date.head())
     print(df_date.columns)
 
-    fig_date = px.line(df_date, x=df_date.index, y='Exercise')
+    fig_date = px.line(df_date, x=df_date.index, y='Exercise', template="plotly_dark")
 
     # Individual exercises per day
     # df_date = df.groupby([df['Date/time'].dt.date], as_index=False)['Exercises'].sum()
     df_date2 = df.groupby([df['Date/time'].dt.date]).sum()
     print(df_date2.head())
 
-    fig_date2 = px.line(df_date2, x=df_date.index, y='Exercises')
+    fig_date2 = px.line(df_date2, x=df_date.index, y='Exercises', template="plotly_dark")
     
     # Raw Daily avg score
     df_avg_score = df.groupby([df['Date/time'].dt.date]).mean()
 
-    fig_avg_score = px.line(df_avg_score, x=df_avg_score.index, y='Score')
+    fig_avg_score = px.line(df_avg_score, x=df_avg_score.index, y='Score', template="plotly_dark")
 
     # Daily Total Score
     df_total_score = df.groupby([df['Date/time'].dt.date])[['Exercises', 'Correct']].sum()
     df_total_score['Total_Score'] = df_total_score['Correct']/df_total_score['Exercises']
     print(df_total_score.head())
 
-    fig_total_score = px.line(df_total_score, x=df_avg_score.index, y='Total_Score')
+    fig_total_score = px.line(df_total_score, x=df_avg_score.index, y='Total_Score', template="plotly_dark")
 
     # Daily Total Score with trend line
     # for regression line, you need 
@@ -47,8 +47,11 @@ def run_viz_app():
     df_total_score['day_count'] = (df_total_score.index - start_date).days
     print(df_total_score['day_count'])
     fig_total_score_trend = px.scatter(df_total_score, x=df_total_score.day_count, y='Total_Score', 
-                                        title="Trend of Daily Scores", trendline='ols',trendline_color_override="black")
+                                        title="Trend of Daily Scores", trendline='ols',trendline_color_override="green", 
+                                        template="plotly_dark")
     r2 = px.get_trendline_results(fig_total_score_trend).px_fit_results.iloc[0].rsquared
+# TODO:     make this text black
+
     fig_total_score_trend.add_annotation(x=1, y=.5,
             text=f"R^2 = {r2}",
             showarrow=False,
@@ -63,6 +66,10 @@ def run_viz_app():
     app.layout = html.Div(children=[html.H1('Teoria Training Progression Dashboard',
                                             style={'textAlign': 'center',
                                                     'color': '#503D36',
+                                                    'font-size': 40}),
+                                    html.H2(f'Total individual exercises completed: {df.Exercises.sum()}',
+                                            style={'textAlign': 'center',
+                                                    'color': '#ccccff',
                                                     'font-size': 40}),
                                     html.P('Exercise Sessions per Day',
                                             style={'textAlign':'center', 'color': '#F57241'}),
